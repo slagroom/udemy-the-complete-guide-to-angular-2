6e0819cd3fbe8f500c58c1e21480a49f1770e49e
slagroom <sl4gr00m@gmail.com>
2019-12-24 09:59:58 -0400

    add recipie-list and recipe-item components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/sh
     HeaderComponent,
     ShoppingListComponent,
     RecipeBookComponent,
-    ShoppingListEditComponent
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
