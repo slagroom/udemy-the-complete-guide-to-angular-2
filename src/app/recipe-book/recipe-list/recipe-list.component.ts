e2b293aa5c0435bfcc0722a895c18231014f78ff
Thomas Showers <tshowers@beyondtrust.com>
2019-12-24 11:24:32 -0400

    recipe item component

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://placekitten.com/g/100/100/'),
+    new Recipe('Another Test', 'This is also simply a test', 'http://placekitten.com/g/150/150/')
+  ];
 
   constructor() { }
 
