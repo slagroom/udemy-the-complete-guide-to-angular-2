3a931ed7bf231d3ba182ef119cc142764949aafe
slagroom <sl4gr00m@gmail.com>
2019-12-29 12:52:02 -0400

    assignment 3 done

--- assignment-3/src/app/app.component.ts
+++ assignment-3/src/app/app.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
 
+class Toggle {
+  public when: number;
+  constructor(public shown: boolean) {
+    if (!Date.now) {
+      Date.now = function() { return new Date().getTime(); }
+    }
+    this.when = Date.now();
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  showDetails: boolean = false;
+  toggles: Toggle[] = [];
+
+  onToggleShowDetails() {
+    this.showDetails = !this.showDetails
+    this.toggles.push(new Toggle(this.showDetails));
+  }
 }
